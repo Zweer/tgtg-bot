039b9d0606966b9b73bb7e333594274d2c437a0a
Zweer <n.olivieriachille@gmail.com>
2021-02-03 17:21:27 +0100

    Re-enabled the setWebhook

--- src/functions/botSetWebhook/handler.ts
+++ src/functions/botSetWebhook/handler.ts
@@ -1,9 +1,7 @@
-// import { bot } from '../common/bot';
+import { bot } from '../common/bot';
 
 const { API_URL } = process.env;
 
 export const main = async () => {
-  console.log(API_URL);
-
-  // await bot.telegram.setWebhook(API_URL);
+  await bot.telegram.setWebhook(API_URL);
 };
