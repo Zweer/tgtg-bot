d133f33b209caecbff002913258e3dc39ee54822
Zweer <n.olivieriachille@gmail.com>
2021-02-03 19:14:42 +0100

    Changing the bot handler, moving away from the lambdaRequestHandler

--- src/functions/bot/handler.ts
+++ src/functions/bot/handler.ts
@@ -4,6 +4,19 @@ import { bot } from '../common/bot';
 
 bot.start((ctx) => ctx.reply('Hello'));
 
-console.log(process.env);
+export const mainn = makeHandler(bot.webhookCallback('/dev/telegram'));
 
-export const main = makeHandler(bot.webhookCallback('/dev/telegram'));
+export const main = async (event) => {
+  console.log(event);
+
+  const body = JSON.parse(event.body);
+
+  console.log(body);
+
+  const success = await bot.handleUpdate(body);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ success }),
+  };
+};
