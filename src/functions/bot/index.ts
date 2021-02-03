98e320b54c70d14962842825f0b1d99953d62b70
Zweer <n.olivieriachille@gmail.com>
2021-02-03 18:49:13 +0100

    Fixed the ssm param

--- src/functions/bot/index.ts
+++ src/functions/bot/index.ts
@@ -2,7 +2,7 @@ import type { AWS } from '@serverless/typescript';
 
 export const path = 'telegram';
 // eslint-disable-next-line no-template-curly-in-string
-export const TELEGRAM_TOKEN = '${ssm:/${self:provider.stage}/${self:service}/telegram~true}';
+export const TELEGRAM_TOKEN = '${ssm:/aws/reference/secretsmanager/${self:provider.stage}/${self:service}/telegram~true}';
 
 const slsFunction: AWS['functions'][string] = {
   handler: `${__dirname.split(process.cwd())[1].substring(1)}/handler.main`,
