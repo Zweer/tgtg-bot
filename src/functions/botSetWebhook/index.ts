4d358fddcc7460c6c20df066f25ebd6084a161f5
Zweer <n.olivieriachille@gmail.com>
2021-02-03 18:36:12 +0100

    Added the token env var

--- src/functions/botSetWebhook/index.ts
+++ src/functions/botSetWebhook/index.ts
@@ -1,10 +1,11 @@
 import type { AWS } from '@serverless/typescript';
 
-import { path } from '../bot';
+import { path, TELEGRAM_TOKEN } from '../bot';
 
 const slsFunction: AWS['functions'][string] = {
   handler: `${__dirname.split(process.cwd())[1].substring(1)}/handler.main`,
   environment: {
+    TELEGRAM_TOKEN,
     API_URL: {
       'Fn::Join': [
         '',
